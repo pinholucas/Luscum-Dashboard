4a11cc7b3dc29e218a2b8fc8eacd3a23694eeebc
Lucas Pinho <erphis@gmail.com>
2021-03-20 21:01:14 -0300

    Initial commit

---
+++ frontend-src/src/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+    <h1>teste</h1>
+    <GlobalStyle/>
+    </>
+  );
+}
---
+++ frontend-src/src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #121214;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
+
+    body {
+        background-color: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;        
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+    
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
