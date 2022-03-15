63f2850f8fa8394cbee801e40fdef79d72b41fd8
Lucas Pinho <erphis@gmail.com>
2022-03-15 02:08:17 -0300

    feat: add entities

---
+++ frontend-src/src/entities.ts
@@ -0,0 +1,31 @@
+export type frameResolution = 'i1080' | 'i1440' | 'i2160';
+export type videoResolution = 'v1080' | 'v1440' | 'v2160';
+
+export type videoData = {
+    firstFrame: {
+        [key in frameResolution]: string;
+    };
+    video: {
+        [key in videoResolution]: string;
+    };
+    attribution: string;
+}
+
+export type BackgroundImageType = {
+    properties: {
+        video: {
+            data: videoData[];
+        },
+        localizedStrings: {
+            video_titles: {
+                [video: string]: string;
+            }
+        }
+    }
+}
+
+export type NTPType = {
+    configs: {
+        'BackgroundImageWC/default': BackgroundImageType;
+    };
+};
