d49c238fd72dfa60817f1adb7f43d164887da194
Lucas Pinho <erphis@gmail.com>
2022-03-15 16:04:54 -0300

    feat: add ReactQueryDevtools

--- frontend-src/src/App.tsx
+++ frontend-src/src/App.tsx
@@ -1,6 +1,7 @@
 import { theme } from "./styles/global";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import Home from "./components/Home";
 
 const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ export function App() {
     <ChakraProvider resetCSS theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Home />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ChakraProvider>
   );
