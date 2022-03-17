e632f891c92040823f0ff3f68e21a4b7e83c8301
Lucas Pinho <erphis@gmail.com>
2022-03-17 02:14:58 -0300

    refac: move weather data formatting to useWeather hook

--- frontend-src/src/components/WeatherWidget.tsx
+++ frontend-src/src/components/WeatherWidget.tsx
@@ -11,124 +11,19 @@ import {
   Icon,
 } from '@chakra-ui/react';
 import { useWeather } from 'hooks/useWeather';
-import { useEffect, useState } from 'react';
 import { locationArrow } from 'utils';
 
-type alertType = {
-  title: string;
-  event: string;
-  abbreviation: string[];
-  desc: string;
-  shortCap: string;
-  severity: string;
-  credit: string;
-  start: string;
-  end: string;
-};
-
-type sourceType = {
-  coordinates: {
-    lat: number;
-    lon: number;
-  };
-  location: {
-    Name: string;
-    StateCode: string;
-    CountryCode: string;
-  };
-};
-
-type weatherData = {
-  alerts: alertType[];
-  temp: number;
-  feels: number;
-  cloudCover: number;
-  umidity: number;
-  dewPoint: number;
-  uv: number;
-  uvDescription: string;
-  windSpeed: number;
-  windDirection: number;
-  caption: string;
-  icon: string;
-  sourceData: sourceType;
-};
-
 export default function WeatherWidget() {
-  const [weatherData, setWeatherData] = useState<weatherData>();
-
   const { data, isLoading, isFetching, error } = useWeather();
-
-  function getWeatherIcon(iconID: number) {
-    switch (iconID) {
-      // Day
-      case 1:
-        return 'SunnyNightV3';
-      case 2:
-        return 'MostlySunnyDay';
-      case 3:
-        return 'PartlyCloudyDayV3';
-      case 4:
-        return 'MostlyCloudyDayV2';
-      case 23:
-        return 'RainShowersDayV2';
-
-      // Night
-      case 28:
-        return 'ClearNightV3';
-      case 29:
-        return 'MostlyClearNight';
-      case 31:
-        return 'MostlyCloudyNightV2';
-      case 50:
-        return 'RainShowersNightV2';
-
-      // Rain & Cloudy
-      case 5:
-        return 'CloudyV3';
-      case 19 || 46: // 19 day, 46 night
-        return 'LightRainV3';
-      case 22 || 49: // 22 day, 49 night
-        return 'ModerateRainV2';
-      case 27:
-        return 'ThunderstormV3';
-
-      default:
-        return 'CloudyV3';
-    }
-  }
-
-  useEffect(() => {
-    if (data) {
-      const weather = data.responses[0].weather[0];
-      const source = data.responses[0].source;
-
-      setWeatherData({
-        alerts: weather.alerts,
-        temp: weather.current.temp,
-        feels: weather.current.feels,
-        cloudCover: weather.current.cloudCover,
-        umidity: weather.current.rh,
-        dewPoint: weather.current.dewPt,
-        uv: weather.current.uv,
-        uvDescription: weather.current.uvDesc,
-        windSpeed: weather.current.windSpd,
-        windDirection: weather.current.windDir,
-        caption: weather.current.cap,
-        icon: getWeatherIcon(weather.current.icon),
-        sourceData: source,
-      });
-    }
-  }, [isLoading, data]);
 
   return (
     <Flex position="absolute" top={4} left={4} alignItems="center" zIndex="3">
       {isLoading && <Spinner color="white" />}
 
       {!isLoading && (
-        <Popover trigger="hover" placement="bottom-start" isOpen>
+        <Popover trigger="hover" placement="bottom-start">
           <PopoverTrigger>
-            <Flex alignItems="center">
+            <Flex alignItems="center" cursor="pointer">
               {isFetching ? (
                 <Spinner color="white" size="sm" />
               ) : (
@@ -136,10 +31,10 @@ export default function WeatherWidget() {
                   <Image
                     width="28px"
                     marginRight={1}
-                    src={`./assets/weather/${weatherData?.icon}.svg`}
+                    src={`./assets/weather/${data?.icon}.svg`}
                   />
                   <Text color="white" fontSize="2xl" textShadow="0 0 2px black">
-                    {error ? '-' : weatherData?.temp}
+                    {error ? '-' : data?.temp}
                   </Text>
                 </>
               )}
@@ -164,28 +59,26 @@ export default function WeatherWidget() {
             <PopoverHeader
               borderColor="secondaryBackground"
               textAlign="center"
-            >{`${weatherData?.sourceData.location.Name}, ${weatherData?.sourceData.location.StateCode}`}</PopoverHeader>
+            >{`${data?.sourceData.location.Name}, ${data?.sourceData.location.StateCode}`}</PopoverHeader>
             <PopoverBody>
-              <Text>{`Clima: ${weatherData?.caption}`}</Text>
-              <Text>{`Sensação térmica: ${weatherData?.feels} °C`}</Text>
+              <Text>{`Clima: ${data?.caption}`}</Text>
+              <Text>{`Sensação térmica: ${data?.feels} °C`}</Text>
               <Text>{`Radiação UV: ${
-                weatherData?.uv
-              } (${weatherData?.uvDescription.toLowerCase()})`}</Text>
-              <Text>{`Umidade: ${weatherData?.umidity}%`}</Text>
+                data?.uv
+              } (${data?.uvDescription.toLowerCase()})`}</Text>
+              <Text>{`Umidade: ${data?.umidity}%`}</Text>
               <Text>
-                {`Vento: ${weatherData?.windSpeed} km/h`}{' '}
+                {`Vento: ${data?.windSpeed} km/h`}{' '}
                 <Icon
                   boxSize={6}
                   viewBox="0 -5 10 24"
-                  transform={`rotate(${
-                    Number(weatherData?.windDirection) - 180
-                  }deg)`}
+                  transform={`rotate(${Number(data?.windDirection) - 180}deg)`}
                   justifyContent="center"
                 >
                   <path d={locationArrow} fill="white" />
                 </Icon>
               </Text>
-              <Text>{`Ponto de orvalho: ${weatherData?.dewPoint} °C`}</Text>
+              <Text>{`Ponto de orvalho: ${data?.dewPoint} °C`}</Text>
             </PopoverBody>
           </PopoverContent>
         </Popover>
--- frontend-src/src/hooks/useWeather.ts
+++ frontend-src/src/hooks/useWeather.ts
@@ -1,13 +1,113 @@
 import { useQuery } from 'react-query';
 import { allOrigins, randomNumber, weatherURL } from 'utils';
 
+type alertType = {
+  title: string;
+  event: string;
+  abbreviation: string[];
+  desc: string;
+  shortCap: string;
+  severity: string;
+  credit: string;
+  start: string;
+  end: string;
+};
+
+type sourceType = {
+  coordinates: {
+    lat: number;
+    lon: number;
+  };
+  location: {
+    Name: string;
+    StateCode: string;
+    CountryCode: string;
+  };
+};
+
+type weatherType = {
+  alerts: alertType[];
+  temp: number;
+  feels: number;
+  cloudCover: number;
+  visibility: number;
+  umidity: number;
+  dewPoint: number;
+  uv: number;
+  uvDescription: string;
+  windSpeed: number;
+  windDirection: number;
+  caption: string;
+  icon: string;
+  sourceData: sourceType;
+};
+
+function getWeatherIcon(iconID: number) {
+  switch (iconID) {
+    // Day
+    case 1:
+      return 'SunnyNightV3';
+    case 2:
+      return 'MostlySunnyDay';
+    case 3:
+      return 'PartlyCloudyDayV3';
+    case 4:
+      return 'MostlyCloudyDayV2';
+    case 23:
+      return 'RainShowersDayV2';
+
+    // Night
+    case 28:
+      return 'ClearNightV3';
+    case 29:
+      return 'MostlyClearNight';
+    case 31:
+      return 'MostlyCloudyNightV2';
+    case 50:
+      return 'RainShowersNightV2';
+
+    // Rain & Cloudy
+    case 5:
+      return 'CloudyV3';
+    case 19 || 46: // 19 day, 46 night
+      return 'LightRainV3';
+    case 22 || 49: // 22 day, 49 night
+      return 'ModerateRainV2';
+    case 27:
+      return 'ThunderstormV3';
+
+    default:
+      return 'CloudyV3';
+  }
+}
+
 export async function getWeatherData() {
   const response = await fetch(
     allOrigins(weatherURL + `&bypass=${randomNumber(1, 999)}`),
   );
   const data = await response.json();
 
-  return data;
+  const weather = data.responses[0].weather[0];
+  const source = data.responses[0].source;
+
+  const weatherData: weatherType = {
+    alerts: weather.alerts,
+    temp: weather.current.temp,
+    feels: weather.current.feels,
+    cloudCover: weather.current.cloudCover,
+    visibility: weather.current.vis,
+    umidity: weather.current.rh,
+    dewPoint: weather.current.dewPt,
+    uv: weather.current.uv,
+    uvDescription: weather.current.uvDesc,
+    windSpeed: weather.current.windSpd,
+    windDirection: weather.current.windDir,
+    caption: weather.current.cap,
+    icon: getWeatherIcon(weather.current.icon),
+    sourceData: source,
+  };
+
+  return weatherData;
 }
 
 export function useWeather() {
