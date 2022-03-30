0ba72ccbcc8a7553e3637d83e2f0665e896e0349
Lucas Pinho <erphis@gmail.com>
2022-03-30 11:46:59 -0300

    fix: improve build extension script

--- frontend-src/buildExtension.js
+++ frontend-src/buildExtension.js
@@ -1,35 +1,49 @@
 const fs = require('fs-extra');
+const path = require('path');
 
-const buildPathFiles = ['build/static', 'build/index.html', 'build/asset-manifest.json'];
-const extensionPathFiles = ['../extension-pack/static', '../extension-pack/index.html', '../extension-pack/asset-manifest.json'];
+const buildPathFiles = [
+  'build/static',
+  'build/assets',
+  'build/index.html',
+  'build/asset-manifest.json',
+  'build/manifest.json',
+];
 
 function removeExtensionOldFiles() {
-    extensionPathFiles.forEach((file) => {
-        fs.rmdirSync(file, {recursive: true});
-        // console.log( '\u001b[32;1mRemoved file: ' + file + '\u001b[0m');
-    });
+  const directory = '../extension-pack';
+
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlinkSync(path.join(directory, file), (err) => {
+        if (err) throw err;
+      });
+
+      console.log('\u001b[32;1mRemoved file: ' + file + '\u001b[0m');
+    }
+  });
 }
 
 function copyExtensionNewFiles() {
-    buildPathFiles.forEach((file) => {
-        const filename  = file.split('/')[1];
-        fs.copySync(file, '../extension-pack/' + filename)
-        // console.log( '\u001b[32;1mCopied file to: ../extension-pack/' + file + '\u001b[0m');
-    });
+  buildPathFiles.forEach((file) => {
+    const filename = file.split('/')[1];
+    fs.copySync(file, '../extension-pack/' + filename);
+    console.log(
+      '\u001b[32;1mCopied file to: ../extension-pack/' + file + '\u001b[0m',
+    );
+  });
 }
 
 function buildExtension() {
-    try {
-        removeExtensionOldFiles();
-        copyExtensionNewFiles();
+  try {
+    removeExtensionOldFiles();
+    copyExtensionNewFiles();
 
-        console.log( '\u001b[32;1mExtension build successfully!\u001b[0m');
-    }
-    catch (error) {
-        console.error(error);
-    }
+    console.log('\u001b[32;1mExtension build successfully!\u001b[0m');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 buildExtension();
-
-  
